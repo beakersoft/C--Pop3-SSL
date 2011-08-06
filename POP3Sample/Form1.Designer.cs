a4e12ae24574fc3fa31a829d83335b54e3f1f45b
beakersoft <beakersoft@gmail.com>
2011-08-06 23:23:44 +0100

    Main Files

---
+++ POP3Sample/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace POP3Sample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grpInfo = new System.Windows.Forms.GroupBox();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.MaskedTextBox();
+            this.txtServer = new System.Windows.Forms.TextBox();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.lblUsername = new System.Windows.Forms.Label();
+            this.lblServer = new System.Windows.Forms.Label();
+            this.lstInfo = new System.Windows.Forms.ListBox();
+            this.cmdConnect = new System.Windows.Forms.Button();
+            this.txtServerPort = new System.Windows.Forms.NumericUpDown();
+            this.lblPort = new System.Windows.Forms.Label();
+            this.grpInfo.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.txtServerPort)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // grpInfo
+            // 
+            this.grpInfo.Controls.Add(this.lblPort);
+            this.grpInfo.Controls.Add(this.txtServerPort);
+            this.grpInfo.Controls.Add(this.txtUsername);
+            this.grpInfo.Controls.Add(this.txtPassword);
+            this.grpInfo.Controls.Add(this.txtServer);
+            this.grpInfo.Controls.Add(this.lblPassword);
+            this.grpInfo.Controls.Add(this.lblUsername);
+            this.grpInfo.Controls.Add(this.lblServer);
+            this.grpInfo.Location = new System.Drawing.Point(12, 12);
+            this.grpInfo.Name = "grpInfo";
+            this.grpInfo.Size = new System.Drawing.Size(818, 106);
+            this.grpInfo.TabIndex = 0;
+            this.grpInfo.TabStop = false;
+            this.grpInfo.Text = "Connection Information";
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(288, 47);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(250, 20);
+            this.txtUsername.TabIndex = 6;
+            this.txtUsername.Text = "beakersoft@gmail.com";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(553, 46);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(250, 20);
+            this.txtPassword.TabIndex = 5;
+            // 
+            // txtServer
+            // 
+            this.txtServer.Location = new System.Drawing.Point(10, 47);
+            this.txtServer.Name = "txtServer";
+            this.txtServer.Size = new System.Drawing.Size(203, 20);
+            this.txtServer.TabIndex = 4;
+            this.txtServer.Text = "pop.gmail.com";
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(559, 30);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(53, 13);
+            this.lblPassword.TabIndex = 2;
+            this.lblPassword.Text = "Password";
+            // 
+            // lblUsername
+            // 
+            this.lblUsername.AutoSize = true;
+            this.lblUsername.Location = new System.Drawing.Point(285, 30);
+            this.lblUsername.Name = "lblUsername";
+            this.lblUsername.Size = new System.Drawing.Size(55, 13);
+            this.lblUsername.TabIndex = 1;
+            this.lblUsername.Text = "Username";
+            // 
+            // lblServer
+            // 
+            this.lblServer.AutoSize = true;
+            this.lblServer.Location = new System.Drawing.Point(7, 30);
+            this.lblServer.Name = "lblServer";
+            this.lblServer.Size = new System.Drawing.Size(69, 13);
+            this.lblServer.TabIndex = 0;
+            this.lblServer.Text = "POP3 Server";
+            // 
+            // lstInfo
+            // 
+            this.lstInfo.FormattingEnabled = true;
+            this.lstInfo.Location = new System.Drawing.Point(12, 124);
+            this.lstInfo.Name = "lstInfo";
+            this.lstInfo.Size = new System.Drawing.Size(818, 134);
+            this.lstInfo.TabIndex = 1;
+            // 
+            // cmdConnect
+            // 
+            this.cmdConnect.Location = new System.Drawing.Point(755, 264);
+            this.cmdConnect.Name = "cmdConnect";
+            this.cmdConnect.Size = new System.Drawing.Size(75, 23);
+            this.cmdConnect.TabIndex = 2;
+            this.cmdConnect.Text = "Connect";
+            this.cmdConnect.UseVisualStyleBackColor = true;
+            this.cmdConnect.Click += new System.EventHandler(this.cmdConnect_Click);
+            // 
+            // txtServerPort
+            // 
+            this.txtServerPort.Location = new System.Drawing.Point(219, 47);
+            this.txtServerPort.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.txtServerPort.Name = "txtServerPort";
+            this.txtServerPort.Size = new System.Drawing.Size(51, 20);
+            this.txtServerPort.TabIndex = 7;
+            this.txtServerPort.Value = new decimal(new int[] {
+            995,
+            0,
+            0,
+            0});
+            // 
+            // lblPort
+            // 
+            this.lblPort.AutoSize = true;
+            this.lblPort.Location = new System.Drawing.Point(216, 30);
+            this.lblPort.Name = "lblPort";
+            this.lblPort.Size = new System.Drawing.Size(26, 13);
+            this.lblPort.TabIndex = 8;
+            this.lblPort.Text = "Port";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(842, 296);
+            this.Controls.Add(this.cmdConnect);
+            this.Controls.Add(this.lstInfo);
+            this.Controls.Add(this.grpInfo);
+            this.Name = "Form1";
+            this.Text = "POP3 Over SSL Sample";
+            this.grpInfo.ResumeLayout(false);
+            this.grpInfo.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.txtServerPort)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grpInfo;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.MaskedTextBox txtPassword;
+        private System.Windows.Forms.TextBox txtServer;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.Label lblUsername;
+        private System.Windows.Forms.Label lblServer;
+        private System.Windows.Forms.ListBox lstInfo;
+        private System.Windows.Forms.Button cmdConnect;
+        private System.Windows.Forms.Label lblPort;
+        private System.Windows.Forms.NumericUpDown txtServerPort;
+    }
+}
+
---
+++ POP3Sample/POP3SSL.cs
@@ -0,0 +1,252 @@
+﻿/***************************************************************************************************************************
+'Author: Luke Niland
+'Language: c#
+'Version: 1.0
+'Description: Class to connent to a pop3 mail server over an SSL connection. Also includes simple functions for getting
+ the number of messages, and extracting header information
+'See RFC 1939 for details of the pop3 specification
+'****************************************************************************************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+using System.IO;
+using System.Globalization;
+using System.Threading;
+
+namespace POP3Sample
+{
+    class POP3SSL
+    {
+        //vars to use for the session
+        public bool isConnected { get; set; }       //have we got a connection
+        public int ErrorLevel { get; set; }         //an error level we can use. 0 is no errors
+        public string ErrorText { get; set; }       //message text of the error        
+
+        private string myServerName;                //servername/address
+        private int myServerPort;                   //server port
+        private string myUserName;                  //username to connect to 
+        private string myPassword;                  //password to use for the user
+        private TcpClient myPopTCPClient;           //socket to handle the connection
+        private SslStream myPopSSLStream;           //SSL stream to talk to the server with
+        private StreamReader mySSLReader;           //stream to read the response's into
+        private StreamWriter mySSLWritter;          //stream to write command to the ssl stream with
+        private string myServerResponse;            //what the server responded with
+
+        public POP3SSL(string ServerName, int ServerPort, string Username, string Password)
+        {
+            //set initial state to not connected, and set the servername, port and user details
+            isConnected = false;
+            myServerName = ServerName;
+            myServerPort = ServerPort;
+            myUserName = Username;
+            myPassword = Password;
+        }
+
+        //try and open the server connection
+        public void Connect()
+        {
+            //this is the complex bit. TCP connection to the server, then create a new ssl connection over that stream, and try and valididate the certs
+            try
+            {
+                myPopTCPClient = new TcpClient(myServerName, myServerPort);
+            }
+            catch (Exception e)
+            {
+                ErrorLevel = 1;
+                ErrorText = "Problem connecting to the pop3 server - " + e.Message;
+                return;
+            }            
+            myPopSSLStream = new SslStream(myPopTCPClient.GetStream(), false, new RemoteCertificateValidationCallback(ValidateServerCertificate), new LocalCertificateSelectionCallback(SelectLocalCertificate));
+
+            //now try and authenticate over the stream
+            myPopSSLStream.AuthenticateAsClient(myServerName);
+
+            //create our reader/writter objects on the stream, so we can talk to it
+            mySSLReader = new StreamReader(myPopSSLStream);
+            mySSLWritter = new StreamWriter(myPopSSLStream);
+
+            //now see what result we got back from the connection
+            myServerResponse = mySSLReader.ReadLine();
+
+            //the response should always start with +OK (the standard pop3 response), then possabley some other info. We will just check for +OK
+            if (myServerResponse.Substring(0, 3) == "+OK")
+                isConnected = true;
+            else
+            {
+                ErrorLevel = 2;
+                ErrorText = "Problem creating SSL Connection. Server Response was - " + myServerResponse;
+                return;
+            }
+
+            //now we have got this far, lets try and send the username and password to login
+            if (!SendCommand("USER " + myUserName))
+            {
+                ErrorLevel = 3;
+                ErrorText = "Problem with user " + ErrorText;
+                return;
+            }
+            if (!SendCommand("PASS " + myPassword))
+            {
+                ErrorLevel = 3;
+                ErrorText = "Problem with password " + ErrorText;
+                return;
+            }            
+        }
+
+        //see how many messages are on the server
+        public int GetNoOfMessages(){
+
+            //first make sure we are connected
+            if (isConnected == false)
+                return 0;
+
+            //send the stat command to the server
+            if (!SendCommand("STAT"))
+                return 0;
+
+            //the response comes back as number of messages then the total size of the messages (in bytes)
+            string[] StatResponse = myServerResponse.Split(' ');
+            return Int32.Parse(StatResponse[1]);
+            
+        }
+
+        //get the content of a message
+        public string GetMessageContent(int MessageIndex)
+        {
+            //first make sure we are connected
+            if (isConnected == false)
+                return string.Empty;
+
+            //use the RETR command with the message number to get the message
+            if (!SendCommand("RETR " + MessageIndex))
+            {
+                ErrorLevel = 1;
+                ErrorText = "Problem getting message content";
+                return "";
+            }
+
+            //the first line we get back is the +OK, the rest of the message needs reading in, up to a single 
+            //line containing only a fullpoint(chr46)
+            string TempHolder;
+            string EmailMessage = "";
+            TempHolder = mySSLReader.ReadLine();
+
+            while (TempHolder != ".")
+            {
+                EmailMessage += TempHolder + Environment.NewLine;
+                TempHolder = mySSLReader.ReadLine();
+            }
+            
+            //return the message
+            return EmailMessage;
+
+        }
+
+        //get a header type out of the email (like the subject)
+        public string ParseMessageHeader(string EmailMessage, string HeaderType)
+        {
+            CultureInfo cultureInfo = Thread.CurrentThread.CurrentCulture;
+            TextInfo textInfo = cultureInfo.TextInfo;
+            int StartPoint,LenOfData;
+            string HeaderData;
+
+            //check for all connatations of the type. Make it all upper, all lower and real case
+            StartPoint = EmailMessage.IndexOf(HeaderType) - 1;
+            if (StartPoint <= 0)            
+                StartPoint = EmailMessage.IndexOf(textInfo.ToTitleCase(HeaderType)) - 1;
+            if (StartPoint <= 0)
+                StartPoint = EmailMessage.IndexOf(textInfo.ToLower(HeaderType)) - 1;
+            if (StartPoint <= 0)
+                StartPoint = EmailMessage.IndexOf(textInfo.ToUpper(HeaderType)) - 1;
+
+            //get the lengh of the string we want
+            LenOfData = EmailMessage.IndexOf(Environment.NewLine, StartPoint) - StartPoint;
+            //now extract the header
+            HeaderData = EmailMessage.Substring(StartPoint, LenOfData);
+
+            //clean the string up a bit
+            return HeaderData.Replace(Environment.NewLine, "");
+        }
+
+        //close the connection to the server, and then distroy our objects
+        public void Disconnect()
+        {
+            //kill the connection from the pop3 server
+            SendCommand("QUIT");
+
+            //now set our status to disconected, and clean up the objects
+            isConnected = false;
+            mySSLReader.Close();
+            mySSLWritter.Close();
+            myPopSSLStream.Close();
+            myPopTCPClient.Close();
+
+        }
+
+        //send a command to the server
+        private bool SendCommand(string POPcommand)
+        {            
+            //send the command, making sure we flush it
+            mySSLWritter.WriteLine(POPcommand);
+            mySSLWritter.Flush();
+
+            //read the response in
+            myServerResponse = mySSLReader.ReadLine();
+
+            //check if the response is null, seems to happen if you send a malformed command
+            if (myServerResponse == null)
+                return false;
+
+            //did we have a good response
+            if (myServerResponse.Substring(0, 3) == "+OK")
+                return true;
+            else
+            {
+                ErrorText = myServerResponse;
+                return false;
+            }
+        }
+
+        //try and find a valid certifcate. Found on the MSDN forums
+        private X509Certificate SelectLocalCertificate(object sender, string targetHost, X509CertificateCollection localCertificates, X509Certificate remoteCertificate, string[] acceptableIssuers)
+        {
+            // Check to see if there are any acceptable issuers and local certificates
+            if (acceptableIssuers != null && acceptableIssuers.Length > 0 && localCertificates != null && localCertificates.Count > 0)
+            {
+                // Loop through the certificates
+                foreach (X509Certificate certificate in localCertificates)
+                {
+                    // Check to see if this cert contains an acceptable issuer
+                    if (Array.IndexOf(acceptableIssuers, certificate.Issuer) != -1)
+                        return certificate;
+                }
+            }
+
+            // If there were no acceptable issuers, just return the first certificate found
+            if (localCertificates != null && localCertificates.Count > 0)
+                return localCertificates[0];
+
+            // Nothing was found
+            return null;
+        }
+
+        //try and validate the cert we have got from the server. Again found on the msdn forums
+        private bool ValidateServerCertificate(object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
+        {
+            // Success
+            if (sslPolicyErrors == SslPolicyErrors.None)
+                return true;
+
+            // Failure
+            return false;
+        }
+
+
+
+
+
+    }
+}
